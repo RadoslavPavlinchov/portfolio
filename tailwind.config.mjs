e23463a4703aea8d01e48d60fe7ee1a699dce558
Radoslav <radoslav.pavlinchov@gmail.com>
2024-09-27 11:24:24 +0300

    feat(wip): create test animation, deploy it

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,12 @@ export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
         extend: {
-            // colors: {},
+            colors: {
+                "main-1": "#3C886E",
+            },
+            backgroundImage: {
+                "hero-pattern": "url('./assets/images/image-7.avif')",
+            },
         },
     },
     plugins: [],
